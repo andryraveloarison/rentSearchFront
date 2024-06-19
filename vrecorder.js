d655cd49b60a08b0e15ddb72a3a9b1315dbe631b
raveloarison <andryraveloarison01@gmail.com>
2024-06-19 08:04:27 +0300

    first commit

---
+++ vrecorder.js
@@ -0,0 +1,144 @@
+// collect DOMs
+const display = document.querySelector('.display')
+const controllerWrapper = document.querySelector('.controllers')
+
+const State = ['Initial', 'Record', 'Download']
+let stateIndex = 0
+let mediaRecorder, chunks = [], audioURL = ''
+
+// mediaRecorder setup for audio
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    console.log('mediaDevices supported..')
+
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    }).then(stream => {
+        mediaRecorder = new MediaRecorder(stream)
+
+        mediaRecorder.ondataavailable = (e) => {
+            chunks.push(e.data)
+        }
+
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(chunks, {'type': 'audio/wav'})
+            chunks = []
+            audioURL = window.URL.createObjectURL(blob)
+            document.querySelector('audio').src = audioURL
+
+
+              // Création de l'objet FormData
+            const formData = new FormData();
+            formData.append('audio', blob);
+
+            // Envoyer l'audio à votre API Flask
+            fetch('http://localhost:5000/transcribe', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                    alert('Réponse de l\'API dans le log'+ data.text);
+                    console.log(data)
+                    // Traiter la réponse ici, par exemple afficher un message ou rediriger l'utilisateur
+                })
+            .catch(error => {
+                    alert('Erreur lors de l\'envoi de l\'audio:', error);
+                    console.log(error)
+            });
+
+                }
+            }).catch(error => {
+                console.log('Following error has occured : ',error)
+            })
+}else{
+    stateIndex = ''
+    application(stateIndex)
+}
+
+const clearDisplay = () => {
+    display.textContent = ''
+}
+
+const clearControls = () => {
+    controllerWrapper.textContent = ''
+}
+
+const record = () => {
+    stateIndex = 1
+    mediaRecorder.start()
+    application(stateIndex)
+}
+
+const stopRecording = () => {
+    stateIndex = 2;
+    mediaRecorder.stop();
+
+    application(stateIndex);
+
+   
+};
+
+const downloadAudio = () => {
+    const downloadLink = document.createElement('a')
+    downloadLink.href = audioURL
+    downloadLink.setAttribute('download', 'audio')
+    downloadLink.click()
+}
+
+const addButton = (id, funString, text) => {
+    const btn = document.createElement('button')
+    btn.id = id
+    btn.setAttribute('onclick', funString)
+    btn.textContent = text
+    controllerWrapper.append(btn)
+}
+
+const addMessage = (text) => {
+    const msg = document.createElement('p')
+    msg.textContent = text
+    display.append(msg)
+}
+
+const addAudio = () => {
+    const audio = document.createElement('audio')
+    audio.controls = true
+    audio.src = audioURL
+    display.append(audio)
+}
+
+const application = (index) => {
+    switch (State[index]) {
+        case 'Initial':
+            clearDisplay()
+            clearControls()
+
+            addButton('record', 'record()', 'Start Recording')
+            break;
+
+        case 'Record':
+            clearDisplay()
+            clearControls()
+
+            addMessage('Recording...')
+            addButton('stop', 'stopRecording()', 'Stop Recording')
+            break
+
+        case 'Download':
+            clearControls()
+            clearDisplay()
+
+            addAudio()
+            addButton('record', 'record()', 'Record Again')
+            break
+
+        default:
+            clearControls()
+            clearDisplay()
+
+            addMessage('Your browser does not support mediaDevices')
+            break;
+    }
+
+}
+
+application(stateIndex)
